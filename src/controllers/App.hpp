26ad829ca3b53b052dd94358d9cd6c2191b74557
Milan <milan.338@outlook.com>
2022-10-10 09:58:03 +1030

    Add antialiasing

--- src/controllers/App.hpp
+++ src/controllers/App.hpp
@@ -5,12 +5,16 @@
 
 #include "./GameController.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/ContextSettings.hpp>
+#include <SFML/Window/WindowStyle.hpp>
 
 class App
 {
   private:
+    sf::ContextSettings _settings = sf::ContextSettings(0, 0, 8, 1, 1, sf::ContextSettings::Default, false);
     // TODO screen resolution
-    sf::RenderWindow _window = sf::RenderWindow(sf::VideoMode(800, 600), "Epic Space Shooter");
+    sf::RenderWindow _window =
+        sf::RenderWindow(sf::VideoMode(800, 600), "Epic Space Shooter", sf::Style::Default, _settings);
     GameController _gameController;
     bool _isInGame;
 
