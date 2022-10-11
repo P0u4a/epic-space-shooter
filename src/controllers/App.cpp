26f350dd27c3c6d49a5c14f832d21a16a7fbfb90
Milan <milan.338@outlook.com>
2022-10-11 21:39:09 +1030

    Add initial test background shader

--- src/controllers/App.cpp
+++ src/controllers/App.cpp
@@ -1,7 +1,12 @@
 #include "App.hpp"
 #include "controllers/GameController.hpp"
+#include "util/FileSystem.hpp"
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Glsl.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Shader.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
 App::App() : _gameController(_window), _isInGame(false)
@@ -24,6 +29,14 @@ int App::beginGameLoop()
     // TODO debug, remove
     this->_isInGame = true;
 
+    // Add starfield shader
+    auto background = sf::RectangleShape(static_cast<sf::Vector2f>(this->_window.getSize()));
+    auto shader = sf::Shader{};
+    if (!shader.loadFromFile(FileSystem::getExecutablePath() + "shaders/starfield.frag", sf::Shader::Fragment))
+        // Error in loading shader
+        return 1;
+    shader.setUniform("u_resolution", static_cast<sf::Glsl::Vec2>(this->_window.getSize()));
+
     // Run main game loop as long as the window is open
     while (this->_window.isOpen())
     {
@@ -40,6 +53,9 @@ int App::beginGameLoop()
         }
         // Clear previous frames
         this->_window.clear(sf::Color::Black);
+
+        // Draw background
+        this->_window.draw(background, &shader);
 
         // Run next game tick if in game
         if (this->_isInGame)
