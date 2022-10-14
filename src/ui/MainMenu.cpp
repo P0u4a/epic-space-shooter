ebc04fa0a0d79e3087585348691ea0e19e70fdb4
P0u4a <pouyamiria@gmail.com>
2022-10-14 22:25:24 +1030

    Make main menu

---
+++ src/ui/MainMenu.cpp
@@ -0,0 +1,28 @@
+#include "MainMenu.hpp"
+#include "controllers/App.hpp"
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/System/Sleep.hpp>
+#include <SFML/System/Time.hpp>
+
+MainMenu::MainMenu(sf::RenderWindow &window)
+    : _window(window), _play(MenuButton(window, 0.4, 0.3, "Press X to Play")), _quit(MenuButton(window, 0.4, 0.7, "Press Esc to Quit"))
+{
+}
+
+void MainMenu::update(float delta_time)
+{
+
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+    {
+        App::setIsInGame(true);
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+    {
+        _window.close();
+    }
+
+    _window.draw(_play.getButton());
+    _window.draw(_quit.getButton());
+}
