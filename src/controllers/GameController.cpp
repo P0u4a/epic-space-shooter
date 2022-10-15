ed62c53bdfbeacf803c17b63eaeca7b3565c1726
P0u4a <pouyamiria@gmail.com>
2022-10-16 02:07:48 +1030

    Add astroid logic to game controller

--- src/controllers/GameController.cpp
+++ src/controllers/GameController.cpp
@@ -1,21 +1,34 @@
 #include "./GameController.hpp"
 #include "game-objects/spaceship/Player.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include "util/FileSystem.hpp"
 
 GameController::GameController(sf::RenderWindow &window)
     : _window(window), _firstPlayer(Player(window, 1000.0, 500.0, 1.5, 3, {10, 10},
                                            {sf::Keyboard::W, sf::Keyboard::A, sf::Keyboard::D})),
       _secondPlayer(
-          Player(window, 1000.0, 500.0, 1.5, 3, {10, 10}, {sf::Keyboard::Up, sf::Keyboard::Left, sf::Keyboard::Right})),
-      _isPaused(false)
+          Player(window, 1000.0, 500.0, 1.5, 3, {10, 10}, {sf::Keyboard::Up, sf::Keyboard::Left, sf::Keyboard::Right})), _isPaused(false)
 {
     auto [window_w, window_h] = static_cast<sf::Vector2f>(window.getSize());
     // Configure player 1 position and colour
-    _firstPlayer.setSprite(sf::Color::Red);
-    _firstPlayer.setPosition(0.25*window_w, 0.5*window_h);
+    this->_firstPlayer.setSprite(sf::Color::Red);
+    this->_firstPlayer.setPosition(0.25 * window_w, 0.5 * window_h);
     // Configure player 2 position and colour
-    _secondPlayer.setSprite(sf::Color::Green);
-    _secondPlayer.setPosition(0.75*window_w, 0.5*window_h);
+    this->_secondPlayer.setSprite(sf::Color::Green);
+    this->_secondPlayer.setPosition(0.75 * window_w, 0.5 * window_h);
+
+    for (int i = 0; i < 4; i++)
+    {
+        this->_asteroids.push_back(Asteroid(window, 300, {(rand() % 3)+1, (rand() % 3) +1}, false));
+    }
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     Vector2f spawn = Asteroid::generateAsteroids();
+    //     this->_asteroids[i].setPosition(spawn.x, spawn.y);
+    // }
+
+
 }
 
 bool GameController::getPaused() const
@@ -41,9 +54,10 @@ void GameController::update(float delta_time)
     this->_firstPlayer.update(delta_time);
     this->_secondPlayer.update(delta_time);
 
-    // Loop through all enemies and update their state
-    for (auto &enemy : this->_enemies)
-        enemy.update(delta_time);
+    // Loop through all asteroids and update their state
+    for (auto &asteroid : this->_asteroids)
+        asteroid.update(delta_time);
+    // this->_asteroid.update(delta_time);
     // Loop through all remaining updateable objects and update their state
     for (auto &updateable : this->_updatables)
         updateable.update(delta_time);
--- src/controllers/GameController.hpp
+++ src/controllers/GameController.hpp
@@ -1,7 +1,7 @@
 #ifndef SRC_CONTROLLERS_GAMECONTROLLER_HPP
 #define SRC_CONTROLLERS_GAMECONTROLLER_HPP
 
-#include "game-objects/spaceship/Enemy.hpp"
+#include "game-objects/spaceship/Asteroid.hpp"
 #include "game-objects/spaceship/Player.hpp"
 #include "interfaces/Updatable.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -13,7 +13,9 @@ class GameController : public Updatable
     sf::RenderWindow &_window;
     Player _firstPlayer;
     Player _secondPlayer;
-    std::vector<Enemy> _enemies;
+    // std::vector<Enemy> _enemies;
+    // Asteroid _asteroid;
+    std::vector<Asteroid>_asteroids;
     std::vector<Updatable> _updatables;
     bool _isPaused;
 
