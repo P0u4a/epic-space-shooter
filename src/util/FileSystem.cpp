68eba1e8c13785eeef9341f332aa8f07f8b3e63b
Milan <milan.338@outlook.com>
2022-10-16 12:45:19 +1030

    Fix compile errors with whereami library

--- src/util/FileSystem.cpp
+++ src/util/FileSystem.cpp
@@ -2,28 +2,11 @@
 #include <climits>
 #include <filesystem>
 #include <string>
-#include <whereami.h>
-
-#if __linux__
-#include <unistd.h>
-#endif
+#include <whereami.hpp>
 
 std::string FileSystem::getExecutablePath()
 {
     std::string path;
-#if __linux__
-    // Temporarily store executable path
-    char buff[PATH_MAX]; // NOLINT(modernize-avoid-c-arrays, cppcoreguidelines-avoid-c-arrays)
-    // Get executable path
-    ssize_t len = readlink("/proc/self/exe", buff, sizeof(buff) - 1);
-    // Successfully retrieved path - store in c++ string
-    if (len != -1)
-    {
-        buff[len] = '\0'; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic,
-                          // cppcoreguidelines-pro-bounds-constant-array-index)
-        path = std::string(std::filesystem::path(buff).remove_filename()) + "/";
-    }
-#else
     // Get path length
     const int length = wai_getExecutablePath(nullptr, 0, nullptr);
     if (length <= 0)
@@ -38,6 +21,5 @@ std::string FileSystem::getExecutablePath()
     path = std::string(std::filesystem::path(buff).remove_filename()) + "/";
     // Cleanup
     delete[] buff;
-#endif
     return path;
 }
